99d4db64cff33207ab5f877dd17d422332c2e179
kaushaljangir <koshaljangir45@gmail.com>
2026-05-05 00:29:03 +0530

    feat: add task feature

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("project started!");
+document.addEventListener('DOMContentLoaded', () => {
+    const taskInput= document.querySelector("#task-input");
+    const addTaskBtn= document.querySelector("#add-task-btn");
+    const taskList= document.querySelector("#task-list");
+    const emptyImg = document.querySelector(".empty-img");
+
+    const toggleEmptyState = () => {
+        emptyImg.style.display = taskList.children.length === 0 ? "block" : "none";
+        };
+
+    const addTask = (event) => {
+        event.preventDefault();
+        const taskText = taskInput.value.trim();
+        if(!taskText){
+            return;
+        }
+
+        const li= document.createElement("li");
+        li.textContent = taskText;
+        taskList.appendChild(li);
+        taskInput.value = "";
+        toggleEmptyState();
+    };
+
+    addTaskBtn.addEventListener("click", addTask);
+    taskInput.addEventListener("keypress", (e) =>{
+        if(e.key === "Enter") {
+            addTask(e);
+        }
+    });
+});
